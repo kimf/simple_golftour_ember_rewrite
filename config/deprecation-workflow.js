59118d4b5d9e4dbf09939adb198f9d2e672f5b48
Kim Fransman <Kim Fransman>
2015-10-14 20:09:00 +0200

    installed ember-cli-eslint and ember-watson

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -2,6 +2,8 @@ window.deprecationWorkflow = window.deprecationWorkflow || {};
 window.deprecationWorkflow.config = {
   workflow: [
     { handler: "silence", matchMessage: "Usage of `Ember.merge` is deprecated, use `Ember.assign` instead." },
+    { handler: "silence", matchMessage: "Using `Application.registry.resolve` is deprecated. Please use `Application.resolveRegistration` instead." }
+
     // { handler: "log", matchMessage: "In Ember 2.0 component factories must have an `isComponentFactory` property set to true. You registered (subclass of Ember.GlimmerComponent) as a component factory. Either add the `isComponentFactory` property to this factory or extend from Ember.Component." }
   ]
 };
