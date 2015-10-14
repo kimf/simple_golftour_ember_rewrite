0e8e003d52a4fabb8f2aacf66d997f0a9d3f6986
Kim Fransman <Kim Fransman>
2015-10-14 20:08:28 +0200

    tour.presentableRole

--- app/models/tour.js
+++ app/models/tour.js
@@ -1,5 +1,18 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  name: DS.attr('string')
+  name: DS.attr('string'),
+  'current-user-role': DS.attr('string'),
+
+  presentableRole: Ember.computed('current-user-role', function() {
+    var role = this.get('current-user-role');
+    switch(role){
+      case 'admin':
+      case 'creator':
+        return '<small>(admin)</small>';
+      default:
+        return '';
+    }
+  })
 });
